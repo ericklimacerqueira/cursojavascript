7b5a6e42934baee54e818179c3b8031dc31dea2e
ericklimacerqueira <ericklimacer@gmail.com>
2021-05-19 14:59:51 -0300

    teste

---
+++ Aula 11/ex0012.js
@@ -0,0 +1,4 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro e ${vel} Km/h`)
+console.log('Você Ultrapassou a velocidade permitida. MULTADO')
+console.log('Dirija sempre usando cinto de segunraça')
